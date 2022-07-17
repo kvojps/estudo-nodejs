ad721d7ed6ccb2420ef0d6ce9d21f97aa637e2fa
Junior <josebazjunior09@gmail.com>
2022-07-16 21:55:41 -0300

    refatorando

--- conceitosBasicos/module/modules.js
+++
@@ -1,8 +0,0 @@
-// const required = require('./people')
-// console.log(required)
-
-const {pessoas, idades} = require('./people')
-console.log(pessoas, idades)
-
-const os = require('os')
-console.log(os.platform(), os.homedir())
--- conceitosBasicos/module/people.js
+++
@@ -1,8 +0,0 @@
-const pessoas = ['jose', 'ferreira', 'santos', 'junior']
-const idades = [18, 19, 20, 21]
-console.log(pessoas)
-
-// module.exports = "bazinga"
-module.exports = {
-    pessoas, idades
-}
