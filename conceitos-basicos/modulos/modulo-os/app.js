f0d25282898b3a2ddca19234f18421621d3b5d6a
Junior <josebazjunior09@gmail.com>
2022-07-17 14:28:58 -0300

    adicionando conteudo sobre o modulo os

--- conceitos-basicos/modulos/modulo-os/app.js
+++ conceitos-basicos/modulos/modulo-os/app.js
@@ -1,3 +1,13 @@
 const os = require('os')
 
-console.log(os.platform(), os.homedir())
+const user = os.userInfo()
+console.log(user)
+console.log(`The System Uptime is ${os.uptime()} seconds`)
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
+}
+console.log(currentOS)
