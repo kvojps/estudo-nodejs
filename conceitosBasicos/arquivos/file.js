59292baaee28ae29d0a46567f3cff36594992dc2
Junior <josebazjunior09@gmail.com>
2022-07-16 21:54:28 -0300

    adicionando operacoes com arquivos node

--- conceitosBasicos/arquivos/file.js
+++ conceitosBasicos/arquivos/file.js
@@ -1,9 +1,21 @@
 const fs = require('fs')
 
-//lendo arquivos
 fs.readFile('./test.txt', (err, data) => {
     if (err) {
         console.log(err)
     }
     console.log(data.toString())
 })
+
+fs.writeFile('./test.txt', 'utilizando o fs.writeFile', () => {
+    console.log('Arquivo foi alterado')
+})
+
+if (!fs.existsSync('./assets')) {
+    fs.mkdir('./assets', (err) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log('Diretório criado')
+    })
+}
