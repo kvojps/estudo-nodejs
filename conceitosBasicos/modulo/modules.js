904295e20ad792b0b111837b32760d71e5b5ae44
Junior <josebazjunior09@gmail.com>
2022-07-16 21:39:10 -0300

    refatorando modules

---
+++ conceitosBasicos/modulo/modules.js
@@ -0,0 +1,8 @@
+// const required = require('./people')
+// console.log(required)
+
+const {pessoas, idades} = require('./people')
+console.log(pessoas, idades)
+
+const os = require('os')
+console.log(os.platform(), os.homedir())
