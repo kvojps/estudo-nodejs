a736d9ba4d5676e32b3c305f4db3e8f3f689bfe2
kvojps <joseferreira.santosjunior@upe.br>
2022-07-18 14:27:47 -0300

    adicionando mais um exemplo sobre o modulo http

---
+++ conceitos-basicos/modulos/modulo-http/exemplo02.js
@@ -0,0 +1,18 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.end('Welcome to our home page')
+    }
+
+    if (req.url === '/about') {
+        for (let i = 0; i < 1000; i++) {
+            for (let j = 0; j < 1000; j++) {
+                console.log(`${i} ${j}`)
+            }
+        }
+        res.end('Welcome to our about page')
+    } 
+})
+
+server.listen(5000)
