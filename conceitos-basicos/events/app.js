35f60d37919eb7f545beb229fa6b986e3b3489b9
kvojps <joseferreira.santosjunior@upe.br>
2022-07-25 15:41:44 -0300

    adicionando conteudos sobre eventos

--- conceitos-basicos/events/app.js
+++ conceitos-basicos/events/app.js
@@ -2,9 +2,11 @@ const EventEmitter = require('events')
 
 const customEmitter = new EventEmitter()
 
+customEmitter.on('response', (name, id) => {
+    console.log(`data received user ${name} with id ${id}`)
+})
 customEmitter.on('response', () => {
-    console.log('data received')
+    console.log('some other logic here')
 })
 
-customEmitter.emit('response')
-customEmitter.emit('response2')
+customEmitter.emit('response', 'jose', 20)
